8e6d8144249729c5bcb9336d4110ab412457a52f
Henrique Segundo <henriquesegundo2000@gmail.com>
2026-06-28 10:39:41 -0300

    Update TelaCadastrarArma.java

--- src/br/com/rpg/telas/TelaCadastrarArma.java
+++ src/br/com/rpg/telas/TelaCadastrarArma.java
@@ -4,6 +4,11 @@ import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class TelaCadastrarArma extends JInternalFrame {
 
@@ -33,6 +38,55 @@ public class TelaCadastrarArma extends JInternalFrame {
 		setBorder(null);
 		setPreferredSize(new Dimension(640, 480));
 		setBounds(0, 0, 640, 480);
+		
+		JLabel lblNewLabel = new JLabel("id");
+		
+		JLabel lblNewLabel_1 = new JLabel("nome");
+		
+		JLabel lblNewLabel_2 = new JLabel("preço");
+		
+		JLabel lblNewLabel_3 = new JLabel("espaços");
+		
+		JLabel lblNewLabel_4 = new JLabel("descrição");
+		
+		JLabel lblNewLabel_5 = new JLabel("Facilidade de uso");
+		
+		JLabel lblNewLabel_6 = new JLabel("Proposito");
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(50)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel_6)
+						.addComponent(lblNewLabel_5)
+						.addComponent(lblNewLabel_3)
+						.addComponent(lblNewLabel_2)
+						.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_4))
+					.addContainerGap(508, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addGap(45)
+					.addComponent(lblNewLabel)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblNewLabel_1)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblNewLabel_2)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblNewLabel_3)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblNewLabel_4)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblNewLabel_5)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblNewLabel_6)
+					.addGap(244))
+		);
+		getContentPane().setLayout(groupLayout);
 
 	}
 
